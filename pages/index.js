831b9bcf0f90816181ca568021fd43fa0e9ad6f6
Masood Rezazadeh <rezazadehmasood@gmail.com>
2021-05-01 15:36:13 +0430

    fix font bugs

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import { MeCard } from '../components/me-card/me-card';
+import Head from 'next/head';
 
 console.log('dcsd');
 
 export default function Home() {
   return (
-    <main>
+    <div>
+      <Head>
+        <title>masood</title>
+      </Head>
       <div className="flex flex-col flex-grow items-center justify-center mt-40">
         <div className="mb-24">
           <MeCard />
         </div>
       </div>
-    </main>
+    </div>
   );
 }
