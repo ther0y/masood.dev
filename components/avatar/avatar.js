e7ea017aff5afd0e6368b2c9f54aed8adc9bb6c1
Masood Rezazadeh <rezazadehmasood@gmail.com>
2021-05-01 20:28:40 +0430

    replaced image

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -1,15 +1,12 @@
 import Image from 'next/image';
-// width = 120,
-//     height = 120,
-//     rounded = false
+
 export const Avatar = ({ className }) => {
   return (
     <Image
-      src="/images/me.png"
+      src="/assets/me.png"
       alt="Picture of the author"
       layout="fill"
-      className={className + ' '}
-      unoptimized={true}
+      className={className}
     />
   );
 };
