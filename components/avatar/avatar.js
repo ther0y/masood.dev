2157ba974c98b3ce556d5f1ee48913da97ce2462
Masood Rezazadeh <rezazadehmasood@gmail.com>
2021-05-01 20:40:56 +0430

    updated nextjs and fixed image optimizer

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -3,10 +3,11 @@ import Image from 'next/image';
 export const Avatar = ({ className }) => {
   return (
     <Image
-      src="/assets/me.png"
+      src="/images/me.png"
       alt="Picture of the author"
       layout="fill"
       className={className}
+      priority
     />
   );
 };
