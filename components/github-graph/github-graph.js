01127cbf016e545f68b24b5e3e8bb7be38ce24a2
Masood Rezazadeh <rezazadehmasood@gmail.com>
2021-05-11 03:18:49 +0430

    Fix Github graph

--- components/github-graph/github-graph.js
+++ components/github-graph/github-graph.js
@@ -10,7 +10,6 @@ function GithubGraph() {
         width={700}
         alt="Masood's Github chart"
         draggable={false}
-        loading="lazy"
       />
       <h4 className="mt-1 text-sm text-gray-400">
         Github activity graph |{' '}
